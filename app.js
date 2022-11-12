2046b679a387359afb1c00ab2133d34fbdc7c0c5
Carolina Diniz (Kaworii) <CarolDinizc21@gmail.com>
2022-11-12 19:09:35 -0300

    Import and set handlebars as view

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 require('dotenv').config()
 const express = require('express')
 const consign = require('consign')
-
+const handlebars = require('express-handlebars')
 const app = express()
+
+// Views
+app.engine('handlebars', handlebars.engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
 
 // Consign
 consign({})
